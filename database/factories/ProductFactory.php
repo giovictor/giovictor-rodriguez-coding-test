bd37632a7a2e61abbb1968ee6da011f11327ab8f
Gio <giorodriguez021@gmail.com>
2023-01-21 22:31:57 +0800

    Created migration, seeder, factory, model and controller for products

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Product::class;
+
+    public function definition()
+    {
+        return [
+            'name' => ucwords(fake()->words(3, true)),
+            'description' => fake()->sentence(),
+            'price' => fake()->randomFloat(2, 1, 9999)
+        ];
+    }
+}
